1015fa0ca6a7968acc6f80b41881e5cb461fdcbf
Zorkijvolk <zorkijvolkmainkraft@gmail.com>
2024-11-18 22:52:24 +0300

    preFinal

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import sqlite3
 import sys
+import os
+import winshell
+from win32com.client import Dispatch
 from PIL import Image
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QTableView, QComboBox, QWizard, QLineEdit
-from PyQt6.QtWidgets import QTextBrowser, QWizardPage, QAbstractItemView, QTextEdit, QMessageBox
+from PyQt6.QtWidgets import QTextBrowser, QWizardPage, QAbstractItemView, QPlainTextEdit, QMessageBox, QFileDialog
 from PyQt6.QtGui import QFont, QPixmap
 from PyQt6.QtCore import Qt
 from PyQt6.QtSql import QSqlDatabase, QSqlTableModel
@@ -18,23 +21,24 @@ class Main(QMainWindow):
         font = QFont()
         font.setFamily("Comic Sans MS")
         self.setGeometry(0, 0, 1500, 750)
+        self.setFixedSize(1500, 750)
         self.setWindowTitle('Справочник Военного РФ')
         self.setStyleSheet('background-color: {}'.format('#e8e6e6'))
         self.aviationButton = QPushButton('Авиация', self)
-        self.fleetButton = QPushButton('Флот', self)
-        self.aviationButton.resize(500, 100)
-        self.aviationButton.move(500, 250)
-        self.aviationButton.setStyleSheet("background-color: {}".format('#fff'))
-        self.fleetButton.setStyleSheet("background-color: {}".format('#1d0be3'))
+        self.fleetButton = QPushButton(self)
         self.fleetButton.resize(500, 100)
-        self.fleetButton.move(500, 400)
-        self.armyButton = QPushButton('Назменая техника', self)
+        self.fleetButton.move(500, 250)
+        self.fleetButton.setStyleSheet("background-color: {}".format('#fff'))
+        self.aviationButton.setStyleSheet("background-color: {}".format('#1d0be3'))
+        self.aviationButton.resize(500, 100)
+        self.aviationButton.move(500, 400)
+        self.aviationButton.setStyleSheet('''font-size: 20pt;
+        background-color: blue;''')
+        self.armyButton = QPushButton(self)
         self.armyButton.setStyleSheet("background-color: {}".format('#e30b0b'))
         self.armyButton.move(500, 550)
         self.armyButton.resize(500, 100)
         self.aviationButton.clicked.connect(self.aviation)
-        self.fleetButton.clicked.connect(self.fleet)
-        self.armyButton.clicked.connect(self.army)
         self.armyButton.setFont(font)
         self.fleetButton.setFont(font)
         self.aviationButton.setFont(font)
@@ -67,10 +71,9 @@ class Main(QMainWindow):
         # И откроем подключение
         self.db.open()
         self.widget = QWizard(self)
-        t = Widget()
-        self.widget.addPage(t)
-        self.widget.resize(750, 500)
-        #        self.widget.show()
+        self.page = Widget()
+        self.widget.addPage(self.page)
+        self.widget.resize(750, 520)
         self.search = QLineEdit(self)
         self.search.setFont(font)
         self.search.setAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -103,22 +106,21 @@ class Main(QMainWindow):
         self.typeSelection.setFont(font)
         self.typeSelection.setStyleSheet('font-size: {}'.format('20pt'))
         self.titleEnter = QLineEdit(self)
-        self.titleEnter.setText('Введите название техники')
+        self.titleEnter.setPlaceholderText('Введите название техники')
         self.titleEnter.resize(420, 60)
         self.titleEnter.move(290, 250)
         self.titleEnter.setFont(font)
         self.titleEnter.setStyleSheet('font-size: {}'.format('20pt'))
         self.titleEnter.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.yearEnter = QLineEdit(self)
-        self.yearEnter.setText('Введите год создания техники')
+        self.yearEnter.setPlaceholderText('Введите год создания техники')
         self.yearEnter.resize(420, 60)
         self.yearEnter.move(720, 250)
         self.yearEnter.setFont(font)
         self.yearEnter.setStyleSheet('font-size: {}'.format('20pt'))
         self.yearEnter.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.infoEnter = QTextEdit(self)
-        self.infoEnter.setText('Введите краткую информацию о технике')
-        self.infoEnter.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.infoEnter = QPlainTextEdit(self)
+        self.infoEnter.setPlaceholderText('Введите краткую информацию о технике')
         self.infoEnter.setFont(font)
         self.infoEnter.setStyleSheet('font-size: {}'.format('20pt'))
         self.infoEnter.resize(1000, 250)
@@ -132,8 +134,51 @@ class Main(QMainWindow):
         self.addButton.setFont(font)
         self.addButton.hide()
         self.addButton.setStyleSheet('font-size: {}'.format('20pt'))
-        self.search.setText('Введите id техники')
+        self.search.setPlaceholderText('Введите id техники')
         self.error = QMessageBox(self)
+        self.widget.setFixedSize(750, 520)
+        file = open('FirstEnter.txt', mode='r', encoding='UTF-8')
+        self.dButton = QPushButton('добавить ярлык на рабочий стол', self)
+        self.dButton.resize(200, 200)
+        self.dButton.move(0, 350)
+        self.dButton.clicked.connect(self.first_enter_f)
+        self.dButton.hide()
+        if file.read() == '0':
+            self.dButton.show()
+
+
+    def first_enter_f(self):
+        self.firstEnter = QWizard()
+        page2 = FirstEnter()
+        page2.yesButton.clicked.connect(self.desktop)
+        page2.noButton.clicked.connect(self.hides)
+        self.firstEnter.addPage(page2)
+        self.firstEnter.resize(750, 520)
+        self.firstEnter.show()
+
+    def hides(self):
+        file = open('FirstEnter.txt', mode='w', encoding='UTF-8')
+        t = '1'
+        file.write(t)
+        file.close()
+        self.firstEnter.hide()
+        self.dButton.hide()
+
+    def desktop(self):
+        desktop = winshell.desktop()
+        t = os.path.abspath('__Main__')[:7]
+        path = os.path.join(desktop, "PilotsHandbook.lnk")
+        target = rf"{t}PilotsHandbook.exe"
+        wdir = rf'{t}'
+        icon = rf"{t}MainImage.png"
+        shell = Dispatch('WScript.Shell')
+        shortcut = shell.CreateShortCut(path)
+        shortcut.Targetpath = target
+        shortcut.WorkingDirectory = wdir
+        shortcut.IconLocation = icon
+        shortcut.save()
+        self.hides()
+
 
     def hide(self):
         self.aviationButton.hide()
@@ -142,46 +187,61 @@ class Main(QMainWindow):
         self.nameProject.hide()
 
     def change_aviation(self):
-        cur = sqlite3.connect("Military_equipment_RF.sqlite").cursor()
+        db = sqlite3.connect("Military_equipment_RF.sqlite")
+        cur = db.cursor()
         if self.typeSelection.currentText() == 'многоцелевой':
-            types = cur.execute(f"""SELECT id from types
+            types = cur.execute(f"""SELECT id from type
                         where title == 'многоцелевой'""").fetchone()[0]
         elif self.typeSelection.currentText() == 'транспортный':
-            types = cur.execute(f"""SELECT id from types
+            types = cur.execute(f"""SELECT id from type
                         where title == 'транспортный'""").fetchone()[0]
         elif self.typeSelection.currentText() == 'учебно-боевой':
-            types = cur.execute(f"""SELECT id from types
+            types = cur.execute(f"""SELECT id from type
                         where title == 'учебно-боевой'""").fetchone()[0]
         elif self.typeSelection.currentText() == 'бомбардировщик':
-            types = cur.execute(f"""SELECT id from types
+            types = cur.execute(f"""SELECT id from type
                         where title == 'Бомбардировщик'""").fetchone()[0]
         elif self.typeSelection.currentText() == 'атакующие':
-            types = cur.execute(f"""SELECT id from types
+            types = cur.execute(f"""SELECT id from type
                         where title == 'Атакующие'""").fetchone()[0]
         elif self.typeSelection.currentText() == 'разведывательные':
-            types = cur.execute(f"""SELECT id from types
+            types = cur.execute(f"""SELECT id from type
                         where title == 'Разведовательные'""").fetchone()[0]
         elif self.typeSelection.currentText() == 'истребитель':
-            types = cur.execute(f"""SELECT id from types
+            types = cur.execute(f"""SELECT id from type
                         where title == 'Истребитель'""").fetchone()[0]
         if self.titleEnter.text():
-            titels = [x[0] for x in cur.execute("""SELECT title from aviation""").fetchall()]
+            pass
+            titeles1 = cur.execute("""SELECT title from aviation""").fetchall()
+            titels = [x[0] for x in titeles1]
             if self.titleEnter.text() in titels:
                 if self.yearEnter.text():
                     if self.yearEnter.text().isdigit():
-                        if self.yearEnter.text() != cur.execute(f"""SELECT year from avitation
-                                                        where title == '{self.titleEnter.text()}'"""):
-                            cur.execute(f"""UPDATE aviation
-                                SET year = '{self.yearEnter}'
-                                WHERE title == '{self.titleEnter.text()}'""")
+                        if int(self.yearEnter.text()) != cur.execute(f"""SELECT year from aviation
+                                                        where title == '{self.titleEnter.text()}'""").fetchone()[0]:
+                            db.execute(f"""UPDATE aviation
+                                SET year = '{self.yearEnter.text()}'
+                                WHERE title == '{self.titleEnter.text()}';""")
+                            db.commit()
+                            self.filter_aviation()
+                        if self.infoEnter.toPlainText():
+                            print('да')
+                            text = open(f'texts/{self.titleEnter.text()}.txt', mode='r', encoding='UTF-8').read()
+                            if self.infoEnter.toPlainText() != text:
+                                text = open(f'texts/{self.titleEnter.text()}.txt', mode='w', encoding='UTF-8')
+                                text.write(self.infoEnter.toPlainText())
+                                text.close()
                     else:
                         self.error.setText('Неправильный формат ввода в поле "год"')
                         self.error.show()
-                if self.infoEnter.toPlainText():
-                    text = open(f'texts/{self.titleEnter.text()}.txt').read()
-                    if self.infoEnter.toPlainText() != text:
-                        text = open(f'texts/{self.titleEnter.text()}.txt', 'wr')
-                        text.write(self.infoEnter.toPlainText())
+                else:
+                    if self.infoEnter.toPlainText():
+                        print('да')
+                        text = open(f'texts/{self.titleEnter.text()}.txt', mode='r', encoding='UTF-8').read()
+                        if self.infoEnter.toPlainText() != text:
+                            text = open(f'texts/{self.titleEnter.text()}.txt', mode='w', encoding='UTF-8')
+                            text.write(self.infoEnter.toPlainText())
+                            text.close()
             else:
                 if not self.yearEnter.text():
                     self.error.setText('Введите год производства техники')
@@ -195,13 +255,22 @@ class Main(QMainWindow):
                             self.error.setText('Введите краткую информацию')
                             self.error.show()
                         else:
-                            image = Image.open(f'images/{self.titleEnter.text()}')
+                            fname = QFileDialog.getOpenFileName(
+                                self, 'Выбрать картинку', '',
+                                'Картинка (*.png);;Все файлы (*)')[0]
+                            image = Image.open(fname)
                             ids1 = cur.execute("""SELECT id from aviation""").fetchall()
                             ids = int([x[0] for x in ids1][-1]) + 1
                             year = self.yearEnter.text()
                             title = self.titleEnter.text()
-                            cur.execute(
-                                f"""INSERT INTO avitation(title, year, type) VALUES('{title}', '{year}', '{types}')""")
+                            image.save(f'images/{title}.png')
+                            db.execute(
+                                f"""INSERT INTO aviation(title, year, type) VALUES('{title}', '{year}', '{types}')""")
+                            db.commit()
+                            self.filter_aviation()
+                            text = open(f'texts/{title}.txt', mode='w', encoding='UTF-8')
+                            text.write(self.infoEnter.toPlainText())
+                            text.close()
 
         else:
             self.error.setText('Введите название техники')
@@ -269,6 +338,7 @@ class Main(QMainWindow):
             if int(self.search.text()) in ids:
                 name = cur.execute(f"""SELECT title from aviation
                             where id == '{int(self.search.text())}'""").fetchone()[0]
+                self.page.set_text_and_image(name)
                 self.widget.show()
             else:
                 self.error.setText('id не найден')
@@ -292,22 +362,6 @@ class Main(QMainWindow):
         self.infoEnter.show()
         self.addButton.show()
 
-    def fleet(self):
-        self.new_page()
-        model = QSqlTableModel(self, self.db)
-        model.setTable('fleet')
-        model.removeColumn(3)
-        model.select()
-        self.first_tabel.setModel(model)
-
-    def army(self):
-        self.new_page()
-        model = QSqlTableModel(self, self.db)
-        model.setTable('army')
-        model.removeColumn(3)
-        model.select()
-        self.first_tabel.setModel(model)
-
 
 class Widget(QWizardPage):
     def __init__(self):
@@ -316,15 +370,55 @@ class Widget(QWizardPage):
 
     def InitUI(self):
         self.text = QTextBrowser(self)
-        self.text.move(0, 0)
+        self.text.move(0, 30)
         self.text.resize(325, 350)
         self.image = QLabel(self)
         self.image.resize(325, 350)
-        self.image.move(325, 0)
+        self.image.move(325, 30)
+        self.label = QLabel(self)
+        self.label.resize(650, 30)
+        self.label.move(0, 0)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label.setStyleSheet('font-size: {}'.format('20pt'))
 
     def set_text_and_image(self, name):
-        self.text.setText(open(f'texts/{name}.txt').read())
+        self.text.setText(open(f'texts/{name}.txt', mode='r', encoding='UTF-8').read())
         self.image.setPixmap(QPixmap(f'images/{name}.png'))
+        self.label.setText(name)
+
+
+class FirstEnter(QWizardPage):
+    def __init__(self):
+        super().__init__()
+        self.InitUI()
+
+    def InitUI(self):
+        self.label = QLabel('Приветствуем вас в нашем приложении!', self)
+        self.yesButton = QPushButton('Да', self)
+        self.noButton = QPushButton('Нет', self)
+        self.label.resize(750, 30)
+        self.label.move(0, 0)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label.setStyleSheet('font-size: {}'.format('20pt'))
+        self.label2 = QLabel('Хотите ли вы создать ярлык на рабочем столе?', self)
+        self.label2.resize(750, 30)
+        self.label2.move(0, 30)
+        self.label2.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label2.setStyleSheet('font-size: {}'.format('20pt'))
+        font = QFont()
+        font.setFamily('Comic Sans MS')
+        self.label.setFont(font)
+        self.label2.setFont(font)
+        self.yesButton.move(0, 120)
+        self.yesButton.resize(315, 50)
+        self.noButton.move(335, 120)
+        self.noButton.resize(315, 50)
+        self.yesButton.setFont(font)
+        self.noButton.setFont(font)
+        self.yesButton.setStyleSheet("""font-size: 20pt;
+        background-color: green""")
+        self.noButton.setStyleSheet("""font-size: 20pt;
+        background-color: red""")
 
 
 if __name__ == '__main__':
